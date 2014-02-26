d07bf7add3ed7f75516c3770e4e943b696b8a5a7
Manjit Singh <manjitsingh23@gmail.com>
2014-02-26 00:34:28 +0000

    fixed spelling error on  Divisible

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,4 @@
 def divisible_by_three?(number)
 	number % 3 == 0
 end
+
--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -6,8 +6,8 @@ describe "divisible by three" do
     expect(divisible_by_three?(3)).to be_true
 end
 
-it "knows that one is not divisable by three" do
-    expect(divisable_by_three?(1)).to be_false
+it "knows that one is not divisible by three" do
+    expect(divisible_by_three?(1)).to be_false
   end
 	
 end
