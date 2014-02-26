813b903f86fb1881effde2b499a8fbe696187e80
Manjit Singh <manjitsingh23@gmail.com>
2014-02-26 00:31:13 +0000

    Added second test- currently failing

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -5,5 +5,9 @@ describe "divisible by three" do
   it "knows that three is divisible by three" do
     expect(divisible_by_three?(3)).to be_true
 end
+
+it "knows that one is not divisable by three" do
+    expect(divisable_by_three?(1)).to be_false
+  end
 	
 end
