52818faf1edee38073c29a86e85b1766963d422d
Manjit Singh <manjitsingh23@gmail.com>
2014-02-26 00:39:06 +0000

    Next test for number 5

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -9,5 +9,12 @@ end
 it "knows that one is not divisible by three" do
     expect(divisible_by_three?(1)).to be_false
   end
+end 
+
+describe "divisible by five" do
 	
+  it "knows that five is divisible by five" do
+    expect(divisible_by_five?(5)).to be_true
+  end
+
 end
